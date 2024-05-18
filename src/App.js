153abf247c0dfb857029de0e01057eb5fef4105d
beastepi <beastepi@d17-428-pc-08.ipa.ii.agh.edu.pl>
2024-05-18 12:02:48 +0200

    Reaktywność

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
-    let email = 'fracz@agh.edu.pl';
+    const [email, setEmail] = useState('fracz@agh.edu.pl');
+
 
 
     function handleChange(event) {
 
-        console.log(event.target.value);
+
+        setEmail(event.target.value);
 
     }
-
+let message;
+if(email.length<10) {
+    message = <div>Masz krótki adres!</div>
+}
+else if(email.length<15){
+    message = <div>Adres jest ok!</div>
+}
+else {
+    message= <div>Adres jest za długi!</div>
+}
 
     return (
 
@@ -19,8 +31,13 @@ function App() {
             <h1>System do zapisów na zajęcia</h1>
 
             <h2>Twój e-mail to {email}.</h2>
+            {message}
+            <input value={email}
+            type="text"
+            onChange={handleChange}
+            className="dlpole"/>
+            <button onClick={()=>alert()}>Wyślij</button>
 
-            <input type="text" onChange={handleChange}/>
 
         </div>
 
